c0a76338e1cd73e9f2c63cba1380b5d31cd129b6
Sergio <illescas.s.m@gmail.com>
2022-09-15 22:40:22 -0300

    form index ok + cambios menores

--- validators/form-index-validator.js
+++ validators/form-index-validator.js
@@ -1,32 +1,43 @@
 const formulario = document.querySelector("[data-form]")
-const inputNombre = document.querySelector("[data-form-nombre]");
-const inputMensaje = document.querySelector("data-form-mensaje");
-const botonEnvio = document.querySelector("[data-form-enviar]");
+const inputNombre = document.querySelector("[data-form = form-nombre]");
+const inputMensaje = document.querySelector("[data-form = form-mensaje]");
+const botonEnvio = document.querySelector("[data-form = enviar]");
 
 
-// formulario.addEventListener('submit', (e) => {
-//     console.log("enviando formulario");
-//     e.preventDefault();
-//     window.location.href = ""
-// })
+formulario.addEventListener('input', () => {
+  inputNombre.setCustomValidity("");
+  inputMensaje.setCustomValidity("");
+  inputNombre.checkValidity();
+  inputMensaje.checkValidity();
+});
 
-
-formulario.addEventListener('submit', validarFormulario)
+inputNombre.addEventListener('invalid', () => {
+  if(inputNombre.value === ""){
+    inputNombre.setCustomValidity("Por favor ingrese su nombre");
+  }
   
+});
+
+inputMensaje.addEventListener('invalid', () => {
+  const mensaje = inputMensaje.value;
+  if(mensaje && mensaje.length < 3) inputMensaje.setCustomValidity("El mensaje debe tener al menos tres caracteres");
+  else if(mensaje === ""){
+    inputMensaje.setCustomValidity("Ingrese un mensaje");
+  } 
   
-function validarFormulario(e) {
-    e.preventDefault();
+});
+
+botonEnvio.addEventListener('click', (e) => {
+  e.preventDefault();
+  inputNombre.checkValidity();
+  inputMensaje.checkValidity();
+  
+  if(inputNombre.validity.valid && inputMensaje.validity.valid){
+    alert("El mensaje se ha enviado correctamente")
+    formulario.submit();
     
-    if(inputNombre.validity) {
-      alert('No has escrito nada en el usuario');
-      return;
-    }
-    var clave = document.getElementById('clave').value;
-    if (clave.length < 6) {
-      alert('La clave no es válida');
-      return;
-    }
-    //this.submit();
-  };
-
-  
+  } else {
+    inputNombre.reportValidity();
+    inputMensaje.reportValidity();
+  }
+});
