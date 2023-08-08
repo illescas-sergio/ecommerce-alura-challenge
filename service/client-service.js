052e9dee0532aa948b1a872a97286beea3ca3213
Sergio Illescas <illescas.s.m@gmail.com>
2023-08-08 10:12:42 -0300

    Update client-service.js
    Editado en github por falta de pull a la pc

--- service/client-service.js
+++ service/client-service.js
@@ -1,33 +1,35 @@
+const URL_FAKEAPI = "https://fakeapi-jxva.onrender.com";
 
-
-const todosLosProductos = () => {
-    return fetch('https://ecommerce-fakeapi-alura.herokuapp.com/producto');
-};
+function todosLosProductos() {
+    return fetch(URL_FAKEAPI + '/producto');
+}
 
 const detalleProducto = (id) => {
-    return fetch(`https://ecommerce-fakeapi-alura.herokuapp.com/producto/?id=${id}`);
+    return fetch(`${URL_FAKEAPI}/producto/?id=${id}`);
 };
 
 const agregarProducto = (name, imageUrl, price, sectionId, description) => {
-    return fetch("https://ecommerce-fakeapi-alura.herokuapp.com/producto", {
+
+    return fetch(URL_FAKEAPI + "/producto", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({name, imageUrl, price, id: uuid.v4(), sectionId, description})
-    });
+    }).then(respuesta => console.log(respuesta))
+    .catch(error => console.log(error));
 };
 
 const modificarProducto = (name, imageUrl, price, sectionId, description, id) => {
-    return fetch(`https://ecommerce-fakeapi-alura.herokuapp.com/producto/${id}`, {
+    return fetch(`${URL_FAKEAPI}/producto/${id}`, {
         method: "PUT",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({name, imageUrl, price, sectionId, description})
     })
-    .then(respuesta => console.log(respuesta))
+    .then(respuesta => respuesta.json()).then(resp => console.log(resp))
     .catch(error => console.log(error));
 };
 
 const eliminarProducto = (id) => {
-    return fetch(`https://ecommerce-fakeapi-alura.herokuapp.com/producto/${id}`, {
+    return fetch(`${URL_FAKEAPI}/producto/${id}`, {
         method: "DELETE"
     });
 };
@@ -40,4 +42,4 @@ export const clienteService = {
     eliminarProducto
 };
     
-    
+     
